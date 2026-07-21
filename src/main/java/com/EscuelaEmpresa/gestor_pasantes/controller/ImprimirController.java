56fbb98ba16aa25a694abaffa9b7d4d5a1c7b9f4
kevinmda <kevin.dure1404@gmail.com>
2026-07-21 20:43:56 -0300

    Generacion de los Pdfs

--- src/main/java/com/EscuelaEmpresa/gestor_pasantes/controller/ImprimirController.java
+++ src/main/java/com/EscuelaEmpresa/gestor_pasantes/controller/ImprimirController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.EscuelaEmpresa.gestor_pasantes.entity.Alumno;
+import com.EscuelaEmpresa.gestor_pasantes.entity.PadreTutor;
 import com.EscuelaEmpresa.gestor_pasantes.entity.Usuario;
 import com.EscuelaEmpresa.gestor_pasantes.repository.AlumnoRepository;
 import com.EscuelaEmpresa.gestor_pasantes.repository.UsuarioRepository;
@@ -30,13 +31,13 @@ public class ImprimirController {
         this.alumnoRepository = alumnoRepository;
     }
 
-    // @GetMapping("/imprimir")
-    // public String imprimir() {
-    //     return "alumno/imprimir";
-    // }
+    @GetMapping("/alumno/imprimir")
+    public String mostrarImprimir() {
+        return "alumno/imprimir";
+    }
 
-    @GetMapping("/imprimir")
-    public void imprimirPdf(Authentication authentication, HttpServletResponse response) throws IOException {
+    @GetMapping("/alumno/imprimir/Contrato.pdf")
+    public void generarPdfContrato(Authentication authentication, HttpServletResponse response) throws IOException {
 
         // 1. Buscar los datos del alumno logueado
         String email = authentication.getName();
@@ -55,18 +56,23 @@ public class ImprimirController {
         PDPageContentStream contentStream = new PDPageContentStream(
                 document, pagina, PDPageContentStream.AppendMode.APPEND, true, true);
 
-        PDType1Font fuente = new PDType1Font(Standard14Fonts.FontName.TIMES_ROMAN);
+        PDType1Font fuente = new PDType1Font(Standard14Fonts.FontName.HELVETICA);
         float tamanioFuente = 10;
 
         // 4. Escribir cada dato en su posición (coordenadas ya calculadas con la grilla)
-        escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getNombres() + " " + alumno.getApellidos(), 411, 860.3f);
-        escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getEspecialidad().getNombre(), 393, 847.1f);
+        escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getNombres() + " " + alumno.getApellidos(), 411, 861);
+        escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getEspecialidad().getNombre(), 393, 848);
         if (alumno.getEmpresa() != null) { //Si ya tiene una empresa asignada entonces si se coloca en el pdf, sino no
-            escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getEmpresa().getNombre(), 203, 821);
+            escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getEmpresa().getNombre(), 203, 821.8f);
         }
         if (alumno.getPadreTutor() != null) { //Si ya tiene un Padre/Tutor entonces si se coloca en el pdf, sino no
-            String nombrePadreTutor = alumno.getPadreTutor().getNombres() + " " + alumno.getPadreTutor().getApellidos();
-            escribirTextoCentrado(contentStream, fuente, tamanioFuente, nombrePadreTutor, 437, 272);
+            PadreTutor pt = alumno.getPadreTutor();
+    
+            String primerNombre = pt.getNombres().trim().split(" ")[0];
+            String primerApellido = pt.getApellidos().trim().split(" ")[0];
+    
+            String nombrePadreTutor = primerNombre + " " + primerApellido;
+            escribirTextoCentrado(contentStream, fuente, tamanioFuente, nombrePadreTutor, 437, 273);
         }
 
         // 5. Cerrar el lienzo (ya no se puede seguir escribiendo después de esto)
@@ -74,7 +80,77 @@ public class ImprimirController {
 
         // 6. Configurar la respuesta HTTP para que el navegador muestre el PDF
         response.setContentType("application/pdf");
-        response.setHeader("Content-Disposition", "inline; filename=ficha_alumno.pdf");
+        response.setHeader("Content-Disposition", "inline; filename=Contrato_alumno.pdf");
+
+        // 7. Enviar el PDF final al navegador
+        document.save(response.getOutputStream());
+        document.close();
+    }
+
+    @GetMapping("/alumno/imprimir/Autorizacion.pdf")
+    public void generarPdfAutorizacion(Authentication authentication, HttpServletResponse response) throws IOException {
+
+        // 1. Buscar los datos del alumno logueado
+        String email = authentication.getName();
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        Alumno alumno = alumnoRepository.findByUsuario_IdUsr(usuario.getIdUsr())
+                .orElseThrow(() -> new RuntimeException("Alumno no encontrado"));
+
+        // 2. Cargar la plantilla PDF
+        File archivoOriginal = new File("src/main/resources/plantillas/AUTORIZACION_PADRES_PEL_25.pdf");
+        PDDocument document = Loader.loadPDF(archivoOriginal);
+        PDPage pagina = document.getPage(0);
+
+        // 3. Abrir el "lienzo" para escribir encima del PDF
+        PDPageContentStream contentStream = new PDPageContentStream(
+                document, pagina, PDPageContentStream.AppendMode.APPEND, true, true);
+
+        PDType1Font fuente = new PDType1Font(Standard14Fonts.FontName.HELVETICA);
+        float tamanioFuente = 10;
+
+        // 4. Escribir cada dato en su posición (coordenadas ya calculadas con la grilla)
+        escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getNombres() + " " + alumno.getApellidos(), 388, 767.2f);
+        escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getCi(), 257, 739);
+        escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getEspecialidad().getNombre(), 156, 719);
+        if (alumno.getEmpresa() != null) { //Si ya tiene una empresa asignada entonces si se coloca en el pdf, sino no
+            escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getEmpresa().getNombre(), 451, 698.9f);
+        }
+        if (alumno.getPadreTutor() != null) { //Si ya tiene un Padre/Tutor entonces si se coloca en el pdf, sino no
+            PadreTutor pt = alumno.getPadreTutor();
+    
+            String primerNombre = pt.getNombres().trim().split(" ")[0];
+            String primerApellido = pt.getApellidos().trim().split(" ")[0];
+    
+            String nombrePadreTutor = primerNombre + " " + primerApellido;
+            escribirTextoCentrado(contentStream, fuente, tamanioFuente, nombrePadreTutor, 307, 640);
+            escribirTextoCentrado(contentStream, fuente, tamanioFuente, pt.getCi(), 135, 617.3f);
+        }
+        //Segunda Autorizacion (la de repuesto)
+        escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getNombres() + " " + alumno.getApellidos(), 388, 366.8f);
+        escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getCi(), 257, 339);
+        escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getEspecialidad().getNombre(), 156, 319);
+        if (alumno.getEmpresa() != null) { //Si ya tiene una empresa asignada entonces si se coloca en el pdf, sino no
+            escribirTextoCentrado(contentStream, fuente, tamanioFuente, alumno.getEmpresa().getNombre(), 451, 298.7f);
+        }
+        if (alumno.getPadreTutor() != null) { //Si ya tiene un Padre/Tutor entonces si se coloca en el pdf, sino no
+            PadreTutor pt = alumno.getPadreTutor();
+    
+            String primerNombre = pt.getNombres().trim().split(" ")[0];
+            String primerApellido = pt.getApellidos().trim().split(" ")[0];
+    
+            String nombrePadreTutor = primerNombre + " " + primerApellido;
+            escribirTextoCentrado(contentStream, fuente, tamanioFuente, nombrePadreTutor, 307, 239.8f);
+            escribirTextoCentrado(contentStream, fuente, tamanioFuente, pt.getCi(), 135, 217.2f);
+        }
+
+        // 5. Cerrar el lienzo (ya no se puede seguir escribiendo después de esto)
+        contentStream.close();
+
+        // 6. Configurar la respuesta HTTP para que el navegador muestre el PDF
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "inline; filename=Autorizacion_alumno.pdf");
 
         // 7. Enviar el PDF final al navegador
         document.save(response.getOutputStream());
